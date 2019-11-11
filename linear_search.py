fad89783c0d780e457071656dbf5f8ab90a268c0
NXNote5130 <nickrhollenbeck@gmail.com>
2019-11-10 18:09:04 -0800

    Setup Linear Search file

--- linear_search.py
+++ linear_search.py
@@ -1,3 +1,32 @@
-import logging
-print('test')
-logging.warning('Watch Out!')
+'''
+Returns the index position of the target if found, else returns None
+'''
+
+
+def linear_search(list, target):
+    for i in range(0, len(list)):
+        if list[i] == target:
+            return i
+    return None
+
+
+'''
+Tests: Fail & Success
+'''
+
+
+def verify(index):
+    if index is not None:
+        print("Target found at index: ", index)
+    else:
+        print("Target not found in list.")
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+result = linear_search(numbers, 12)
+verify(result)
+
+
+result = linear_search(numbers, 6)
+verify(result)
